30ba9372c72024a2067816d0fa1aff3a56e5d784
Narasimha.MurthyDV <nmurthydv@deloitte.com>
2023-05-27 22:19:16 +0530

    My first Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Hello, This is <b>Narasimha</b>. Welcome to my first go live app.
+        </p>
+        <p>
+        It's just a start. I'll be keep on updating the app.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/narasimha-m-24458517b"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
